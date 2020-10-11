f4e6929298b0e35f70c637526866c981bf51c2bc
AZC <albertzcheng@gmail.com>
2020-10-11 09:09:32 -0700

    added handling error cases for user input of password length

--- js/script.js
+++ js/script.js
@@ -1,23 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//GIVEN I need a new, secure password
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN prompted for character types to include in the password
-//THEN I choose lowercase, uppercase, numeric, and/or special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
-// Write password to the #password input
-
 function generatePassword() {
   var psCriteria = [];
   var psCharString = "";
@@ -25,9 +8,23 @@ function generatePassword() {
   var psCharStringArr = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMOPQRSTUVWXYZ", "1234567890", " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"];
   var psLength = prompt("For length of password, please enter a number between 8 and 128");
 
-  if (psLength < 8 || psLength > 128) {
-    passwordResult = "Error:  Password generation failed. \n Please generate password again and enter a number BETWEEN 8 and 128 for password length.";
+  if (isNaN(psLength)) {
+
+    //Handles the case if user does not enter a number for password length
+    passwordResult = "ERROR:  Password generation failed. \n Please generate password again and enter a NUMBER for password length.";
+
+  } else if (!Number.isInteger(psLength)) {
+
+    //Handles the case if the user does not enter a whole number for password length 
+    passwordResult = "ERROR:  Password generation failed. \n Please generate password again and enter a number that is a WHOLE NUMBER for password length.";
+
+  } else if (psLength < 8 || psLength > 128) {
+
+    //Handles the case if user doe not enter a number between 8 and 128
+    passwordResult = "ERROR:  Password generation failed. \n Please generate password again and enter a number BETWEEN 8 and 128 for password length.";
+
   } else {
+
     //Stores user answer for including lowercase in password criteria
     psCriteria[0] = confirm("Do you want password character type to include lowercase?");
     //Stores user answer for including uppercase in password criteria
@@ -36,6 +33,8 @@ function generatePassword() {
     psCriteria[2] = confirm("Do you want password character type to include numeric?");
     //Stores user answer for including special characters in password criteria
     psCriteria[3] = confirm("Do you want password character type to include special characters?");
+
+    //Ceate the string that we will randomly select characters from for password
     for (let i = 0; i < psCharStringArr.length; i++) {
       if (psCriteria[i]) {
         psCharString = psCharString.concat(psCharStringArr[i]);
@@ -43,16 +42,20 @@ function generatePassword() {
     }
 
     if (psCharString === "") {
-      passwordResult = "Error:  Password generation failed. \n Please generate password again and select at least one criteria:  lowercase, uppercase, numeric, and/or special characters.";
+
+      //Handles the case if the user does not select any password criteria
+      passwordResult = "ERROR:  Password generation failed. \n Please generate password again and select at least one criteria:  lowercase, uppercase, numeric, and/or special characters.";
+
     } else {
+
       for (let i = 0; i < psLength; i++) {
         let randomNum = Math.round(Math.random() * psCharString.length);
+        //Generates the password
         passwordResult = passwordResult.concat(psCharString[randomNum]);
+
       }
     }
   }
-
-
   return passwordResult;
 }
 
